fec88eb9f84c2ac4c111f2242c911e856d16b58b
Yann Bizeul <yann@tynsoe.org>
2025-02-09 08:57:30 +0100

    Fix port argument handling

--- public_folders/run.sh
+++ public_folders/run.sh
@@ -5,6 +5,6 @@ set +u
 export PORT=$(bashio::addon.port 8080)
 export OPTIONS="./data/options.json"
 
-bashio::log.info "Starting http service on port $PORT."
+bashio::log.info "Starting http service on port $PORT (external port is " $(bashio::addon.port) "."
 
 exec npm run start;
